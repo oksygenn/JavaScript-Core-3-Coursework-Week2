60ad267b959f100ea08f696cbe77d1bab47c723b
Oksana Kostenko <kseniavlad31@gmail.com>
2021-03-24 18:06:26 +0000

    completes challenges/programmer-humour

---
+++ challenges/programmer-humour/script.js
@@ -0,0 +1,26 @@
+const API = "https://xkcd.now.sh/?comic=latest";
+
+const getPicture = () => {
+  const body = document.querySelector("body");
+  fetch(API)
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw response.statusText;
+      }
+    })
+    .then((obj) => {
+      const image = document.createElement("img");
+      image.src = obj.img;
+      body.append(image);
+    })
+    .catch((error) => {
+      const h1 = document.createElement("h1");
+      h1.innerText = `Oops! Something went wrong :( ${error}`;
+      h1.style.color = "red";
+      body.append(h1);
+    });
+};
+
+window.onload = getPicture;
