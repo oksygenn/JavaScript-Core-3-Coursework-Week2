21b072e9fca47496fdd0c3f872d92df1cb8ba36a
Oksana Kostenko <kseniavlad31@gmail.com>
2021-03-24 16:56:51 +0000

    changes dog-photo js

--- challenges/dog-photo-gallery/script.js
+++ challenges/dog-photo-gallery/script.js
@@ -1,22 +1,29 @@
 const URL = "https://dog.ceo/api/breeds/image/random";
 
+const setup = () => {
+  const button = document.querySelector(".get-photo");
+  button.addEventListener("click", getPicture);
+  getPicture();
+};
+
 const getPicture = () => {
+  fetch(URL)
+    .then((response) => response.json())
+    .then((obj) => {
+      render(obj);
+    });
+};
+
+const render = (obj) => {
   const body = document.querySelector("body");
   const list = document.querySelector(".photo-list");
   body.append(list);
-  const button = document.querySelector(".get-photo");
-
-  button.addEventListener("click", () => {
-    fetch(URL)
-      .then((response) => response.json())
-      .then((obj) => {
-        const liEl = document.createElement("li");
-        const image = document.createElement("img");
-        image.src = obj.message;
-        liEl.append(image);
-        list.append(liEl);
-      });
-  });
+  const liEl = document.createElement("li");
+  const image = document.createElement("img");
+  image.src = obj.message;
+  image.setAttribute("width", 300);
+  liEl.append(image);
+  list.append(liEl);
 };
 
-getPicture();
+setup();
